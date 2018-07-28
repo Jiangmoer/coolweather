0ac272fda7bc88dfeb2b03a29b700a746ff69d80
Ting Jiang <ting.jiang@ericsson.com>
2018-07-29 00:27:20 +0800

    add weather info show function

--- app/src/main/java/com/example/ejianti/coolweather/util/Utility.java
+++ app/src/main/java/com/example/ejianti/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.ejianti.coolweather.db.City;
 import com.example.ejianti.coolweather.db.County;
 import com.example.ejianti.coolweather.db.Province;
+import com.example.ejianti.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -68,5 +70,18 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return  null;
     }
 }
